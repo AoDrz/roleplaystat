fd2ca8121660fbef8337288ac43d7e21e022ac40
AoDrz <aoduriez@gmail.com>
2020-06-04 12:09:57 +0200

    premier commit

---
+++ calcule.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+/*
+function contr (){
+
+    var ment = 40;
+
+    var aff = document.getElementById("affect").value;
+    var int = document.getElementById("inter").value;
+    var feb = document.getElementById("febri").value;  
+    var col = document.getElementById("coler").value;
+    
+
+    var bete = (ment + ( aff  - int - feb - col));
+
+        if( bete > 38 ){
+            document.getElementById("control").innerHTML = bete*2;
+            var con = bete*2;
+            g.refresh(con);
+        }
+        else if (bete > 20 ){
+            document.getElementById("control").innerHTML = bete*1.5;
+        }
+        else if ( bete > 10){
+            document.getElementById("control").innerHTML = bete*1.2;
+        }
+        else{
+            document.getElementById("control").innerHTML = bete*0.5;
+        }
+}
+*/
+
+
+
+
